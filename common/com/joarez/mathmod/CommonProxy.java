e9c64984826010694feda514ee5650bd10ae7bca
Unknown <jhonatas.rezende@hotmail.com>
2017-09-23 16:07:03 -0300

    Added Recipes initialization

--- common/com/joarez/mathmod/CommonProxy.java
+++ common/com/joarez/mathmod/CommonProxy.java
@@ -1,5 +1,6 @@
 package com.joarez.mathmod;
 
+import init.ModBlocks;
 import init.ModItems;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -7,11 +8,13 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class CommonProxy {
 	public void preInit(FMLPreInitializationEvent event) {
+		ModBlocks.init();
 		ModItems.init();
 	}
 	
 	public void init(FMLInitializationEvent event) {
-		
+		ModBlocks.initRecipes();
+		ModItems.initRecipes();
 	}
 	
 	public void postInit(FMLPostInitializationEvent event) {
