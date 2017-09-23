b1575c57aacace99d7cea6346495cc2fb38e765d
Unknown <jhonatas.rezende@hotmail.com>
2017-09-23 16:07:26 -0300

    Added recipe

--- common/com/joarez/mathmod/items/ItemTutorial.java
+++ common/com/joarez/mathmod/items/ItemTutorial.java
@@ -3,17 +3,31 @@ package com.joarez.mathmod.items;
 import com.joarez.mathmod.MathMod;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ItemTutorial extends Item {
 	
+	
+	public ItemTutorial() {
+		setMaxStackSize(64);
+		setCreativeTab(MathMod.tabTutorial); //make item appears in creative tab
+	}
+	
+	public void addRecipes() {
+		GameRegistry.addShapelessRecipe(new ItemStack(this), Items.BONE,Items.BONE, new ItemStack(Blocks.LOG,1,2));
+			
+	}
+	
 	@Override
-	public String getUnlocalizedName(ItemStack stack) {
+	public String getUnlocalizedName(ItemStack stack) {		
 		return "item."+ MathMod.RESOURCE_PREFIX + Names.TUTORIAL_ITEM;
 	}
 	
@@ -26,3 +40,4 @@ public class ItemTutorial extends Item {
 	}
 	
 }
+		
